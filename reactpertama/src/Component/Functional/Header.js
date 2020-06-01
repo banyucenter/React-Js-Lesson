0619f497e3f4b7278063bdbf465f72405cecf157
Ipung Purwono <ipungofficial@gmail.com>
2020-06-01 11:28:46 +0700

    Arrow Function

--- reactpertama/src/Component/Functional/Header.js
+++ reactpertama/src/Component/Functional/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import logo from '../../logo.svg';
 
-function Header() {
+const Header = () => {
     return <header className="App-header">
     <img src={logo} className="App-logo" alt="logo" />
     <p>
